6b8b2010e5136f4e928e27ed41bd260456d4bf9e
killedmyself <killedmyself@users.noreply.github.com>
2018-04-13 13:50:24 +0300

    commit

---
+++ Introduction/1.1.1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Set first number : ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Set second number : ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bigger value is : " + (num1 > num2 ? num1 : num2)) ;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Introduction/1.1.2/1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Set sound of an animal: ");
+            string str = Console.ReadLine();
+            if (str == "mew")
+            {
+                Console.WriteLine("Feed the cat !");
+            }
+            else if (str == "woof")
+            {
+                Console.WriteLine("Feed the dog !");
+            }
+            else
+            {
+                Console.WriteLine("Wrong input .");
+            }
+            Console.ReadKey();
+        }
+    }
+}
