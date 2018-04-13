9214e6c4f726b78b130835544bfddbecdbcd8247
taktashev <38331400+taktashev@users.noreply.github.com>
2018-04-13 14:53:52 +0300

    Ivan's commit

---
+++ Introduction/1.1.5/1.1.5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите день недели: ");
+                int.TryParse(Console.ReadLine(), out int day);
+                switch (day)
+                {
+                    case 1:
+                        Console.WriteLine("Понедельник");
+                        break;
+                    case 2:
+                        Console.WriteLine("Вторник");
+                        break;
+                    case 3:
+                        Console.WriteLine("Среда");
+                        break;
+                    case 4:
+                        Console.WriteLine("Четверг");
+                        break;
+                    case 5:
+                        Console.WriteLine("Пятница");
+                        break;
+                    case 6:
+                        Console.WriteLine("Суббота");
+                        break;
+                    case 7:
+                        Console.WriteLine("Воскресенье");
+                        break;
+                    default:
+                        Console.WriteLine("Попробуйте еще раз!");
+                        continue;
+                }
+                break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Introduction/1.2.5/1.2.5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            double result = 0;
+            Console.WriteLine("Введите 5 целых чисел: ");
+
+            Console.CursorTop++;
+
+            for (int i = 0; i < 5; i++)
+            {
+                while (true)
+                {
+                    Console.Write("Введите число: ");
+
+                    if (!int.TryParse(Console.ReadLine(), out numbers[i]))
+                    {
+                        Console.WriteLine("Попробуйте еще раз!");
+                        continue;
+                    }
+
+                    break;
+                }
+
+                result += numbers[i];
+            }
+
+            result /= 5;
+
+            Console.CursorTop++;
+
+            Console.Write($"Среднее арифметическое ваших чисел = {result}");
+            Console.ReadKey();
+        }
+    }
+}
