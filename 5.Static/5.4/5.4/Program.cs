0e855d90b359dc1daafa9159ffd1246a9bf8f1f3
taktashev <38331400+taktashev@users.noreply.github.com>
2018-04-15 19:22:39 +0300

    Nikita's tasks

---
+++ 5.Static/5.4/5.4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                ShowUI();
+            }
+        }
+
+        static void ShowUI()
+        {
+            Console.WriteLine("Выбор метода: ");
+            Console.WriteLine(" 1. MySubstring(int, int)");
+            Console.WriteLine(" 2. MyIndexOf(string)");
+            Console.WriteLine(" 3. MyReplace(string, string)");
+            Console.Write("Ввод: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("-----------------------------");
+
+            bool canInvoke = true;
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("Получить новую строку путём извлечения заданного количества символов, начиная с заданного индекса");
+                    Console.Write("Строка:              ");
+                    string str1 = Console.ReadLine();
+                    Console.Write("Индекс:              ");
+                    if (!int.TryParse(Console.ReadLine(), out int index))
+                        canInvoke = false;
+                    Console.Write("Количество символов: ");
+                    if (!int.TryParse(Console.ReadLine(), out int count))
+                        canInvoke = false;
+
+                    if (canInvoke)
+                        Console.WriteLine($"Вывод:               {str1.MySubstring(index, count)}");
+                    else
+                        Console.WriteLine("Некорректные аргументы");
+
+                    ExitFromCast();
+                    break;
+                case "2":
+                    Console.WriteLine("Получить индекс первого вхождения подстроки в строку");
+                    Console.Write("Строка:            ");
+                    string str2 = Console.ReadLine();
+                    Console.Write("Искомая подстрока: ");
+                    string find = Console.ReadLine();
+                    Console.WriteLine($"Вывод:             {str2.MyIndexOf(find)}");
+
+                    ExitFromCast();
+                    break;
+                case "3":
+                    Console.WriteLine("Заменить все вхождения подстроки в строке на новую подстроку");
+                    Console.Write("Строка:                              ");
+                    string str3 = Console.ReadLine();
+                    Console.Write("Подстрока, которую следует заменить: ");
+                    string oldValue = Console.ReadLine();
+                    Console.Write("Подстрока, которую следует вставить: ");
+                    string newValue = Console.ReadLine();
+                    Console.WriteLine($"Вывод:                               {str3.MyReplace(oldValue, newValue)}");
+
+                    ExitFromCast();
+                    break;
+            }
+        }
+        static void ExitFromCast()
+        {
+            Console.WriteLine("\nНажмите любую кнопку, чтобы продолжить...");
+            Console.ReadKey();
+
+            Console.WriteLine("=============================");
+        }
+    }
+}
---
+++ 5.Static/5.4/5.4/StringExtensions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace _5._4
+{
+    static class StringExtensions
+    {
+        public static string MySubstring(this string str, int index, int count)
+        {
+            if(str != null && index > -1 && count > 0)
+            {
+                string newString = "";
+
+                try
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        newString += str[index + i];
+                    }
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Ура, исключение! - " + e.Message + " :з");
+                }
+
+                return newString;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static int MyIndexOf(this string str, string value)
+        {
+            int index = -1;
+            if(str != null && value != null && value.Length > 0 && str.Length >= value.Length)
+            {
+                for(int i = 0; i < str.Length; i++)
+                {
+                    string temp = "";
+
+                    for (int j = 0; j < value.Length; j++)
+                    {
+                        try
+                        {
+                            temp += str[i + j];
+                        }
+                        catch(IndexOutOfRangeException e) { }
+                    }
+
+                    if (value == temp)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+
+            return index;
+        }
+        public static string MyReplace(this string str, string oldValue, string newValue)
+        {
+            if(oldValue != null && newValue != null && oldValue != "" && newValue != "")
+            {
+                for(int i = 0; i < str.Length; i++)
+                {
+                    string temp = "";
+
+                    for (int j = 0; j < oldValue.Length; j++)
+                    {
+                        try
+                        {
+                            temp += str[i + j];
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+
+                    if(temp == oldValue)
+                    {
+                        str = str.Remove(i, oldValue.Length);
+                        str = str.Insert(i, newValue);
+                        i = -1;
+                    }
+                }
+
+                return str;
+            }
+            else
+            {
+                return str;
+            }
+        }
+    }
+}
