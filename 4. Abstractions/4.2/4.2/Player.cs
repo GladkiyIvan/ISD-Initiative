ca7122b889bbdc7a5652d008b28bf12273687465
taktashev <38331400+taktashev@users.noreply.github.com>
2018-04-15 13:47:59 +0300

    Nikita's tasks

---
+++ 4. Abstractions/4.2/4.2/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4._2
+{
+    class Player : IPlayable, IRecodable
+    {
+        void IPlayable.Play()
+        {
+            Console.WriteLine("Статус: воспроизведение");
+        }
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Статус: воспр. приостановлено");
+        }
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Статус: воспр. прекращенo");
+        }
+        void IRecodable.Record()
+        {
+            Console.WriteLine("Статус: запись");
+        }
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("Статус: запись приостановлена");
+        }
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("Статус: запись прекращена");
+        }
+    }
+}
