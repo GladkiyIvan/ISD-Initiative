9b90645a6e51fea9c3091c06a8af5168a473e57a
Nikita <taktashev.n@gmail.com>
2018-04-16 17:00:24 +0300

    Nikita's tasks

---
+++ 6.Structures/6.6/6.6/Accauntant.cs
@@ -0,0 +1,10 @@
+﻿namespace _6._6
+{
+    static class Accauntant
+    {
+        public static bool AskForBonus(Post post, int hours)
+        {
+            return hours > (int)post ? true : false;
+        }
+    }
+}
