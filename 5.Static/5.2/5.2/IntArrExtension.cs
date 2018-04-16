a8eece247eca82a93e440654755ac94dc02818ca
Ivan <liberte1793@gmail.com>
2018-04-16 18:15:25 +0300

    Ivan's task

---
+++ 5.Static/5.2/5.2/IntArrExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2
+{
+    public static class IntArrExtension
+    {
+        public static int[] QuickSort(this int[] arr, int firstIndex , int lastIndex )
+        {
+            int first = firstIndex;
+            int last = lastIndex;
+            int piv = arr[(firstIndex + lastIndex) / 2];
+
+            while (first <= last)
+            {
+                while (arr[first] > piv)
+                {
+                    first++;
+                }
+                while (arr[last] < piv)
+                {
+                    last--;
+                }
+                if (first <= last)
+                {
+                    int tmp = arr[first];
+                    arr[first] = arr[last];
+                    arr[last] = tmp;
+
+                    first++;
+                    last--;
+                }
+            }
+            if (firstIndex < last)
+            {
+                QuickSort(arr, firstIndex, last);
+            }
+            if (first < lastIndex)
+            {
+                QuickSort(arr, first, lastIndex);
+            }
+            return arr;
+        }
+    }
+}
---
+++ 5.Static/5.2/5.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+
+            while (true)
+            {
+                Console.Write("Введите размерность массива: ");
+
+                if (!int.TryParse(Console.ReadLine(), out length) || length <= 0)
+                {
+                    Console.WriteLine("Некорректный ввод. Попробуйте еще раз!");
+                    continue;
+                }
+                break;
+            }
+
+            int[] arr = new int[length];
+
+            Console.CursorTop++;
+
+            for (int i = 0; i < length; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Введите {i + 1}-й элемент массива: ");
+
+                    if (!int.TryParse(Console.ReadLine(), out arr[i]))
+                    {
+                        Console.WriteLine("Некорректный ввод. Попробуйте еще раз!");
+                        continue;
+                    }
+                    break;
+                }
+            }
+
+            Console.CursorTop++;
+            arr = arr.QuickSort(0, arr.Length - 1);
+
+            Console.Write("Ваш отсортированный массив: ");
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
