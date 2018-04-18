e112592d0a796a20d1d5e7bb4099020d7df4f3c3
Ivan <liberte1793@gmail.com>
2018-04-18 21:17:49 +0300

    Ivan's Tasks

---
+++ 8.Generics/8.1/8.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = MyClass<int>.FacrotyMethod();
+            Console.WriteLine($"Значение по умолчанию для типа int: {myInt}");
+            double myDouble = MyClass<double>.FacrotyMethod();
+            Console.WriteLine($"Значение по умолчанию для типа double: {myDouble}");
+            string myString = MyClass<string>.FacrotyMethod();
+            Console.WriteLine($"Значение по умолчанию для типа string: {myString}");
+            Object myObject = MyClass<Object>.FacrotyMethod();
+            Console.WriteLine($"Значение по умолчанию для типа object: {myObject}");
+            Console.ReadKey();
+        }
+    }
+}
