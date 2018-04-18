be5868b9bb4b384ea8ea3ed41c6003d3031ff70b
YakovMatvienko <yakovmatvienko@gmail.com>
2018-04-18 16:36:20 +0300

    Yakov's commit

---
+++ 5.Static/5.3/5.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 5, 145, -5 };
+            arr.Sort(true);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            int[] arr1 = { 19, 37, -3, 3, 91, -322 };
+            arr1.Sort(false);
+            Console.WriteLine("");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.Write(arr1[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5.Static/5.3/5.3/Sorter.cs
@@ -0,0 +1,81 @@
+﻿namespace _5._3
+{
+    static class Sorter
+    {
+        public static void Sort(this int[] arr, bool whatWay)
+        {
+            if (whatWay == true)
+            {
+                SortAscending(arr, 0, arr.Length - 1);
+            }
+            else
+            {
+                SortDescending(arr, 0, arr.Length - 1);
+            }
+        }
+        static void SortDescending(int []a, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            int num = a[start];
+
+            int i = start, j = end;
+
+            while (i < j)
+            {
+                while (i < j && a[j] < num)
+                {
+                    j--;
+                }
+
+                a[i] = a[j];
+
+                while (i < j && a[i] > num)
+                {
+                    i++;
+                }
+
+                a[j] = a[i];
+            }
+
+            a[i] = num;
+            SortDescending(a, start, i - 1);
+            SortDescending(a, i + 1, end);
+        }
+        static void SortAscending(int []a, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            int num = a[start];
+
+            int i = start, j = end;
+
+            while (i < j)
+            {
+                while (i < j && a[j] > num)
+                {
+                    j--;
+                }
+
+                a[i] = a[j];
+
+                while (i < j && a[i] < num)
+                {
+                    i++;
+                }
+
+                a[j] = a[i];
+            }
+
+            a[i] = num;
+            SortAscending(a, start, i - 1);
+            SortAscending(a, i + 1, end);
+        }
+    }
+}
