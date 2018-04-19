102a57cb97f0ad33691df3d9d1b3b0e355d19a06
Ivan <liberte1793@gmail.com>
2018-04-19 15:11:47 +0300

    Ivan task

---
+++ 8.Generics/8.2/8.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Haldkiy");
+            myList.Add("Ivan");
+            Console.WriteLine($"Ваш лист: {myList[0]} {myList[1]}");
+            Console.WriteLine($"Количество элементов в сипске = {myList.Count}");
+            Console.ReadKey();
+        }
+    }
+}
