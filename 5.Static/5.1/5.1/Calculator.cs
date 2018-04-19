a75917c8e77b393ca93101722f46a572b9055bd0
killedmyself <killedmyself@users.noreply.github.com>
2018-04-19 19:12:31 +0300

    Mark's tasks

---
+++ 5.Static/5.1/5.1/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5._1
+{
+    class Calculator
+    {
+        static public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        static public double Derogation(double a, double b)
+        {
+            return a - b;
+        }
+        static public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+        static public double Division(double a, double b)
+        {
+
+            if (double.IsInfinity(a / b)) { Console.WriteLine("Infinity"); return double.PositiveInfinity ; }
+            else return a/b ;
+            
+        }
+        static public double Power(double a, double power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= a;
+            }
+            return result;
+        }
+    }
+}
---
+++ 5.Static/5.1/5.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set 2 numbers : ");
+            if (!double.TryParse(Console.ReadLine(), out double x))
+            {
+                Console.WriteLine("Wrong input");
+                Console.ReadKey();
+                return;
+            }
+            if (!double.TryParse(Console.ReadLine(), out double y))
+            {
+                Console.WriteLine("Wrong input");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine(x + " + " + y + " = " + Calculator.Add(x, y));
+            Console.WriteLine(x + " - " + y + " = " + Calculator.Derogation(x, y));
+            Console.WriteLine(x + " * " + y + " = " + Calculator.Multiplication(x, y));
+            Console.WriteLine(x + " / " + y + " = " + Calculator.Division(x, y));
+            Console.WriteLine(x + " ^ " + y + " = " + Calculator.Power(x, y));
+            
+            Console.ReadKey();
+        }
+    }
+}
