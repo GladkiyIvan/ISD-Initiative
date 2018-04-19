64f9ce03d17e28bc462c88a514a321b32b7552bd
Ivan <liberte1793@gmail.com>
2018-04-19 16:15:06 +0300

    Ivan's task

---
+++ 9.Collection/9.2/9.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Three Rings for the Elven-kings under the sky,");
+            myList.Add("Seven for the Dwarf-lords in their halls of stone,");
+            myList.Add("Nine for Mortal Men doomed to die,");
+            myList.Add("One for the Dark Lord on his dark throne");
+            myList.Add("In the Land of Mordor where the Shadows lie.");
+            myList.Add("One Ring to rule them all, One Ring to find them,");
+            myList.Add("One Ring to bring them all and in the darkness bind them");
+            myList.Add("In the Land of Mordor where the Shadows lie....");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            foreach(string quote in myList)
+            {
+                Console.WriteLine(quote);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
