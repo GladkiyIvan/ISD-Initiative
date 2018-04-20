9f7dd337f2b81aaf0158f76aa03d50fa74e178af
YakovMatvienko <yakovmatvienko@gmail.com>
2018-04-20 16:06:32 +0300

    Yakov's commit

---
+++ 9.Collection/9.1/9.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9._1
+{
+    class Program
+    {
+        public static IEnumerable<int> PairNumbers(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    yield return array[i];
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 4, 8, 12, 10, 9, 3, 5, 14 };
+            IEnumerable<int> pairArray =  PairNumbers(array);
+            Console.ReadKey();
+        }
+    }
+}
