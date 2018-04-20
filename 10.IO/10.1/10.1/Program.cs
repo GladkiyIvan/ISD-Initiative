8d12f80b48e6e5f79f998fd7a7d8940d129c62d1
Ivan <liberte1793@gmail.com>
2018-04-20 11:53:48 +0300

    Ivan's task

---
+++ 10.IO/10.1/10.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System;
+
+namespace _10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appPath = AppDomain.CurrentDomain.BaseDirectory;
+            DirectoryInfo dirInfo;
+
+            try
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    dirInfo = new DirectoryInfo($@"{appPath}/MyBins/Folder_{i}");
+                    dirInfo.Create();
+                }
+
+                Console.WriteLine("Ваши папки были созданы в директории: Debug/MyBins/...");
+                Console.WriteLine("Нажмите любую клавишу, чтоб удалить эти папки.");
+
+                Console.ReadKey();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    dirInfo = new DirectoryInfo($@"{appPath}/MyBins/Folder_{i}");
+                    dirInfo.Delete();
+                }
+
+                Console.WriteLine("Папки были удалены.");
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Что-то пошло не так: {e.ToString()}");
+            }
+        }
+    }
+}
