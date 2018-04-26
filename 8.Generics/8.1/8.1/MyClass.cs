e78042275509a9d040eaecc59e172df7ad26b351
taktashev <38331400+taktashev@users.noreply.github.com>
2018-04-26 09:36:39 +0300

    Ivan's and Nikita's tasks (updated)

--- 8.Generics/8.1/8.1/MyClass.cs
+++ 8.Generics/8.1/8.1/MyClass.cs
@@ -6,29 +6,7 @@ namespace _8._1
     {
         public static T FactoryMethod(params object[] args)
         {
-            if (args.Length == 0 && default(T) != null)
-            {
-                return default(T);
-            }
-            else
-            {
-                try
-                {
-                    return (T)Activator.CreateInstance(typeof(T), args);
-                }
-                catch(Exception e)
-                {
-                    Console.WriteLine("Ошибка: " + e.Message);
-                    Console.WriteLine("Вероятно, вы пытались создать экземпляр типа, передавая его специальному конструктору некорректные параметры");
-
-                    if(default(T) == null)
-                        Console.WriteLine("Метод вернул null");
-                    else
-                        Console.WriteLine("Метод вернул " + default(T));
-
-                    return default(T);
-                }
-            }
+            return (T)Activator.CreateInstance(typeof(T), args);
         }
     }
 }
--- 8.Generics/8.1/8.1/Program.cs
+++ 8.Generics/8.1/8.1/Program.cs
@@ -8,27 +8,69 @@ namespace _8._1
         {
             Console.SetWindowSize(126, 26);
 
-            Console.WriteLine("Метод MyClass<T>.Factory(params object[] args):");
-            Console.WriteLine("\n - если при вызове метода ему не передаются параметры и Т не является ссылочным типом, то метод вернёт\n    значение по умолчанию для типа Т;");
-            Console.WriteLine("\n - если необходимо создать экземпляр типа через пользовательский конструктор, аргументы метода должны\n    соответствовать параметрам интересующего пользовательского конструктора;");
-            Console.WriteLine("\n - при попытке передачи в параметрах аргументов, не соответствующих ни одному из пользовательских конструкторов\n    типа, возникшее исключение будет обработано.");
+            Console.WriteLine("Метод MyClass<T>.Factory(params object[] args)");
 
             Console.WriteLine("\n" + new string('-', 126));
 
-            int i = MyClass<int>.FactoryMethod();
-            Console.WriteLine("Тип, полученый в результате операции MyClass<int>.FactoryMethod().............................." + i.GetType().ToString() + "\n");
+            int i;
+            try
+            {
+                i = MyClass<int>.FactoryMethod();
+                Console.WriteLine("Тип, полученый в результате операции MyClass<int>.FactoryMethod().............................." + i.GetType().ToString() + "\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+                Console.WriteLine("Вероятно, вы пытались создать экземпляр типа, передавая его специальному конструктору некорректные параметры");
+            }
 
-            string s = MyClass<string>.FactoryMethod('a', 1);
-            Console.WriteLine("Тип, полученый в результате операции MyClass<string>.FactoryMethod('a', 1)....................." + s.GetType().ToString() + "\n");
+            string s;
+            try
+            {
+                s = MyClass<string>.FactoryMethod('a', 1);
+                Console.WriteLine("Тип, полученый в результате операции MyClass<string>.FactoryMethod('a', 1)....................." + s.GetType().ToString() + "\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+                Console.WriteLine("Вероятно, вы пытались создать экземпляр типа, передавая его специальному конструктору некорректные параметры");
+            }
 
-            ClassWithOnlyDefaultCtor defaultCtor = MyClass<ClassWithOnlyDefaultCtor>.FactoryMethod();
-            Console.WriteLine("Тип, полученый в результате операции MyClass<ClassWithOnlyDefaultCtor>.FactoryMethod()........." + defaultCtor.GetType().ToString() + "\n");
+            ClassWithOnlyDefaultCtor defaultCtor;
+            try
+            {
+                defaultCtor = MyClass<ClassWithOnlyDefaultCtor>.FactoryMethod();
+                Console.WriteLine("Тип, полученый в результате операции MyClass<ClassWithOnlyDefaultCtor>.FactoryMethod()........." + defaultCtor.GetType().ToString() + "\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+                Console.WriteLine("Вероятно, вы пытались создать экземпляр типа, передавая его специальному конструктору некорректные параметры");
+            }
 
-            ClassWithoutDefaultCtor withoutDefaultCtor = MyClass<ClassWithoutDefaultCtor>.FactoryMethod("a", 1);
-            Console.WriteLine("Тип, полученый в результате операции MyClass<ClassWithoutDefaultCtor>.FactoryMethod('a', 1)...." + withoutDefaultCtor.GetType().ToString() + "\n");
+            ClassWithoutDefaultCtor withoutDefaultCtor;
+            try
+            {
+                withoutDefaultCtor = MyClass<ClassWithoutDefaultCtor>.FactoryMethod("a", 1);
+                Console.WriteLine("Тип, полученый в результате операции MyClass<ClassWithoutDefaultCtor>.FactoryMethod('a', 1)...." + withoutDefaultCtor.GetType().ToString() + "\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+                Console.WriteLine("Вероятно, вы пытались создать экземпляр типа, передавая его специальному конструктору некорректные параметры");
+            }
 
             Console.WriteLine("Сообщение об ошибке, полученное в результате операции MyClass<int>.FactoryMethod('five'): ");
-            int wrongCtorParams = MyClass<int>.FactoryMethod("five");
+            int wrongCtorParams;
+            try
+            {
+                wrongCtorParams = MyClass<int>.FactoryMethod("five");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+                Console.WriteLine("Вероятно, вы пытались создать экземпляр типа, передавая его специальному конструктору некорректные параметры");
+            }
 
             Console.ReadKey();
         }
